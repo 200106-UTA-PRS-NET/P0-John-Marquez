cd1eb5f2fc984b843ee0ca699c8a95a043c7b670
JohnMarquez <35536915+JohnMarquez@users.noreply.github.com>
2020-01-13 23:06:03 -0600

    UI

--- PizzaBox/PizzaBox.Client/Program.cs
+++ PizzaBox/PizzaBox.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace PizzaBox.Client
 {
@@ -6,7 +7,98 @@ namespace PizzaBox.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Clear();
+
+                string userName;
+                string password;
+                Console.WriteLine("Welcome to Marquez's Pizzaria");
+                Console.Write("Username: ");
+                userName = Console.ReadLine();
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+
+                if (userName == "john" && password == "password")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Username or Password");
+                    Thread.Sleep(2000);
+
+                }
+
+                
+
+            }
+
+            while(true)
+            {
+                Console.Clear();
+                int location;
+                Console.WriteLine("Hello");
+                Console.WriteLine("1. Revature");
+                Console.WriteLine("2. Liv+");
+                Console.WriteLine("3. UTA");
+                Console.Write("Please Select a Location: ");
+                location = Convert.ToInt32(Console.ReadLine());
+
+                if (location < 3 && location > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Choice");
+                    Thread.Sleep(2000);
+                }
+
+            }
+
+            while(true)
+            {
+                Console.Clear();
+                int select;
+                Console.WriteLine("Hello");
+                Console.WriteLine("1. Place an Order");
+                Console.WriteLine("2. View Order History");
+                Console.WriteLine("3. Sign Out");
+                Console.Write("Please select an option: ");
+                select = Convert.ToInt32(Console.ReadLine());
+
+                if (select < 3 && select > 0)
+                {
+                    //break;
+                    if(select == 1)
+                    {
+                        int pizza;
+                        Console.WriteLine("Hello");
+                        Console.WriteLine("1. Meat Lovers");
+                        Console.WriteLine("2. Cheese");
+                        Console.WriteLine("3. Pepperoni");
+                        Console.Write("Please Select a Pizza: ");
+                        pizza = Convert.ToInt32(Console.ReadLine());
+
+                        if (pizza < 3 && pizza > 0)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Select Crust and Size");
+                            Console.WriteLine("");
+                          
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Choice");
+                    Thread.Sleep(2000);
+                }
+
+            }
+
+
         }
     }
 }
